4a3797fb21f5ec7bd6532830df47b39155d87e2e
Srecko Morovic <srecko.morovic@cern.ch>
2015-08-12 21:46:47 +0200

    updates

--- node-server/Readme.md
+++ node-server/Readme.md
@@ -4,6 +4,13 @@ npm install express -save
 npm install elasticsearch -save
 npm install node-php -save
 
+#installing oracle package:
+#sudo yum install oracle-instantclient-devel
+export OCI_LIB_DIR=/usr/lib64/oracle/*/client/lib64
+export OCI_INC_DIR=/usr/include/oracle/*/client
+npm install oracledb -save
+
+
 This is base web server directory.
 copy F3Mon (browser page) "dist" directory to web (in installation target directory) and rename it to node-f3mon
 
--- rpm/node-rpm.sh
+++ rpm/node-rpm.sh
@@ -21,9 +21,9 @@ cd $TOPDIR
 # we are done here, write the specs and make the fu***** rpm
 cat > fff-node-scripts.spec <<EOF
 Name: $PACKAGENAME
-Version: 0.90
+Version: 0.91
 Release: 0
-Summary: hlt daemon
+Summary: node.js f3mon scripts
 License: gpl
 Group: DAQ
 Packager: smorovic
@@ -34,8 +34,10 @@ AutoReqProv: no
 Requires: npm 
 
 Provides:/opt/node/prod/
+Provides:/opt/node/test/
 Provides:/etc/init.d/fff-node-server
-Provides:/opt/node/prod/node-service
+Provides:/etc/init.d/test-fff-node-server
+Provides:/opt/node/node-daemon.js
 Provides:/etc/logrotate.d/fff-node-server
 
 %description
@@ -49,13 +51,16 @@ rm -rf \$RPM_BUILD_ROOT
 mkdir -p \$RPM_BUILD_ROOT
 %__install -d "%{buildroot}/opt/node"
 %__install -d "%{buildroot}/opt/node/prod"
+%__install -d "%{buildroot}/opt/node/test"
 %__install -d "%{buildroot}/var/log/node"
 
 mkdir -p %{buildroot}/opt/node/prod
+mkdir -p %{buildroot}/opt/node/test
 mkdir -p %{buildroot}/etc/init.d
 mkdir -p %{buildroot}/etc/logrotate.d
 cp $BASEDIR/fff-node-server %{buildroot}/etc/init.d/fff-node-server
-cp $BASEDIR/node-service.sh %{buildroot}/opt/node/prod/node-service.sh
+cp $BASEDIR/test-fff-node-server %{buildroot}/etc/init.d/test-fff-node-server
+cp $BASEDIR/node-daemon.js %{buildroot}/opt/node/node-daemon.js
 cp $BASEDIR/logrotate-node %{buildroot}/etc/logrotate.d/fff-node-server
 
 %files
@@ -63,40 +68,64 @@ cp $BASEDIR/logrotate-node %{buildroot}/etc/logrotate.d/fff-node-server
 #/opt/fff
 #%attr( 755 ,root, root) /etc/
 #%attr( 755 ,root, root) /opt/
-#%attr( 700 ,root, root) /opt/node/
-%attr( 700 ,root, root) /var/log/node
-%attr( 700 ,root, root) /opt/node/prod
+#%attr( 755 ,root, root) /opt/node/
+%attr( 755 ,root, root) /var/log/node
+%attr( 755 ,root, root) /opt/node/prod
+%attr( 755 ,root, root) /opt/node/test
 %attr( 755 ,root, root) /etc/init.d/fff-node-server
-%attr( 755 ,root, root) /opt/node/prod/node-service.sh
+%attr( 755 ,root, root) /etc/init.d/test-fff-node-server
+%attr( 755 ,root, root) /opt/node/node-daemon.js
 %attr( 755 ,root, root) /etc/logrotate.d/fff-node-server
 
 %post
 #echo "post install trigger"
-/etc/init.d/fff-node-server stop
-unlink /cmsnfses-web/es-web/prod/app.js
-unlink /cmsnfses-web/es-web/prod/node_modules
-unlink /cmsnfses-web/es-web/prod/web
+/etc/init.d/fff-node-server stop || true
+/etc/init.d/test-fff-node-server stop >& /dev/null || true
+unlink /opt/node/prod/app.js || true
+unlink /opt/node/prod/node_modules || true
+unlink /opt/node/prod/web || true
 
-ln -s /cmsnfses-web/es-web/prod/app.js app.js
-ln -s /cmsnfses-web/es-web/prod/node_modules node_modules
-ln -s /cmsnfses-web/es-web/prod/web web
+ln -s /cmsnfses-web/es-web/prod/app.js /opt/node/prod/app.js
+ln -s /cmsnfses-web/es-web/prod/node_modules /opt/node/prod/node_modules
+ln -s /cmsnfses-web/es-web/prod/web /opt/node/prod/web
+
+
+unlink /opt/node/test/app.js || true
+unlink /opt/node/test/node_modules || true
+unlink /opt/node/test/web || true
+
+ln -s /cmsnfses-web/es-web/test/app.js /opt/node/test/app.js
+ln -s /cmsnfses-web/es-web/test/node_modules /opt/node/test/node_modules
+ln -s /cmsnfses-web/es-web/test/web /opt/node/test/web
+
+
 #set user ownership
-/usr/sbin/useradd es-cdaq-runtime -g es-cdaq || true
+/usr/sbin/useradd es-cdaq-runtime -g es-cdaq -s /sbin/nologin || true
 #chown es-cdaq-runtime:es-cdaq -R /opt/node/*.log || true
 #chown es-cdaq-runtime:es-cdaq -R /var/log/node/*.log || true
  
 chown es-cdaq-runtime:es-cdaq -R /var/run/
 /etc/init.d/fff-node-server start
-chkconfig --add fff-node-start
+chkconfig --add fff-node-server
 
 %preun
 #echo "pre uninstall trigger"
 if [ \$1 == 0 ]; then 
-  /etc/init.d/fff-node-server stop
-  chkconfig --del fff-node-server
-  rm -rf /var/run/fff-node-server.pid
-  rm -rf /var/lock/subsys/fff-node-server
+  /etc/init.d/fff-node-server stop || true
+  /etc/init.d/test-fff-node-server stop >& /dev/null || true
+  chkconfig --del fff-node-server || true
+  rm -rf /var/run/*fff-node-server.pid
+  #rm -rf /var/lock/subsys/fff-node-server
   /usr/sbin/userdel es-cdaq-runtime || true
+
+  unlink /opt/node/prod/app.js || true
+  unlink /opt/node/prod/node_modules || true
+  unlink /opt/node/prod/web || true
+
+  unlink /opt/node/test/app.js || true
+  unlink /opt/node/test/node_modules || true
+  unlink /opt/node/test/web || true
+
 fi
 
 
