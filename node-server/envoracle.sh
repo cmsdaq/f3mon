79d444c35553bf0cd324e9f0dc79c733e135c486
Srecko Morovic <srecko.morovic@cern.ch>
2016-10-17 14:06:39 +0200

    *fix eporting for oracle compilation *daemonize patch format fixed

--- node-server/envoracle.sh
+++ node-server/envoracle.sh
@@ -1,6 +1,6 @@
 OCI_INC_DIR="/usr/include/oracle/*/client"
 OCI_LIB_DIR="/usr/lib64/oracle/*/client/lib64"
-OCI_INC_DIR=`echo $OCI_INC_DIR`
-OCI_LIB_DIR=`echo $OCI_LIB_DIR`
+export OCI_INC_DIR=`echo $OCI_INC_DIR`
+export OCI_LIB_DIR=`echo $OCI_LIB_DIR`
 #set -f
 #set +f
