07d42fba985686d7f406467070ec66a267325811
Srecko <smorovic@gmail.com>
2017-01-19 16:30:23 +0100

    environment setup (cc7)

--- node-server/envoracle.sh
+++ node-server/envoracle.sh
@@ -1,6 +1,15 @@
-OCI_INC_DIR="/usr/include/oracle/*/client"
-OCI_LIB_DIR="/usr/lib64/oracle/*/client/lib64"
-export OCI_INC_DIR=`echo $OCI_INC_DIR`
-export OCI_LIB_DIR=`echo $OCI_LIB_DIR`
+#new dir hierarchy for oracle packages in CC7:
+export OCI_INC_DIR=`echo /usr/include/oracle/*/client64`
+export OCI_LIB_DIR=`echo /usr/lib/oracle/*/client64/lib`
+
+setenv OCI_INC_DIR `echo /usr/include/oracle/*/client64`
+setenv OCI_LIB_DIR `echo /usr/lib/oracle/*/client64/lib`
+
+#use for SLC6
+#OCI_INC_DIR="/usr/include/oracle/*/client"
+#OCI_LIB_DIR="/usr/lib64/oracle/*/client/lib64"
+#export OCI_INC_DIR=`echo $OCI_INC_DIR`
+#export OCI_LIB_DIR=`echo $OCI_LIB_DIR`
+
 #set -f
 #set +f
