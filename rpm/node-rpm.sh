0ae351c64ea673f9eee064e2ecc70f49b82c4ba5
Srecko Morovic <srecko.morovic@cern.ch>
2017-03-29 19:01:40 +0200

    fixed logrotate and rpm script

--- rpm/node-rpm.sh
+++ rpm/node-rpm.sh
@@ -33,10 +33,10 @@ BuildArch: $BUILD_ARCH
 AutoReqProv: no
 Requires: npm,nodejs,gcc-c++,pam-devel,oracle-instantclient-devel
 
-Provides:/opt/node/
-Provides:/opt/node/prod/
-Provides:/opt/node/priv/
-Provides:/opt/node/test/
+Provides:/opt/f3mon/
+Provides:/opt/f3mon/prod/
+Provides:/opt/f3mon/priv/
+Provides:/opt/f3mon/test/
 Provides:/usr/lib/systemd/system/f3mon.service
 Provides:/usr/lib/systemd/system/f3mon.priv.service
 Provides:/usr/lib/systemd/system/f3mon.test.service
@@ -51,18 +51,18 @@ init scripts for F3Mon Node.js server
 %install
 rm -rf \$RPM_BUILD_ROOT
 mkdir -p \$RPM_BUILD_ROOT
-%__install -d "%{buildroot}/opt/node"
-%__install -d "%{buildroot}/opt/node/prod"
-%__install -d "%{buildroot}/opt/node/priv"
-%__install -d "%{buildroot}/opt/node/test"
-%__install -d "%{buildroot}/var/log/node"
-%__install -d "%{buildroot}/var/log/node/prod"
-%__install -d "%{buildroot}/var/log/node/priv"
-%__install -d "%{buildroot}/var/log/node/test"
+%__install -d "%{buildroot}/opt/f3mon"
+%__install -d "%{buildroot}/opt/f3mon/prod"
+%__install -d "%{buildroot}/opt/f3mon/priv"
+%__install -d "%{buildroot}/opt/f3mon/test"
+%__install -d "%{buildroot}/var/log/f3mon"
+%__install -d "%{buildroot}/var/log/f3mon/prod"
+%__install -d "%{buildroot}/var/log/f3mon/priv"
+%__install -d "%{buildroot}/var/log/f3mon/test"
 %__install -d "%{buildroot}/usr/lib/systemd/system"
-mkdir -p %{buildroot}/opt/node/prod
-mkdir -p %{buildroot}/opt/node/priv
-mkdir -p %{buildroot}/opt/node/test
+mkdir -p %{buildroot}/opt/f3mon/prod
+mkdir -p %{buildroot}/opt/f3mon/priv
+mkdir -p %{buildroot}/opt/f3mon/test
 mkdir -p %{buildroot}/etc/logrotate.d
 mkdir -p %{buildroot}/usr/lib/systemd/system
 cp $BASEDIR/f3mon.service %{buildroot}/usr/lib/systemd/system/
@@ -73,18 +73,18 @@ cp $BASEDIR/logrotate-node %{buildroot}/etc/logrotate.d/f3mon
 %files
 %defattr(-, root, root, -)
 #/opt/fff
-%attr( 755 ,root, root) /var/log/node
-%attr( 755 ,root, root) /var/log/node/prod
-%attr( 755 ,root, root) /var/log/node/priv
-%attr( 755 ,root, root) /var/log/node/test
-%attr( 755 ,root, root) /opt/node/
-%attr( 755 ,root, root) /opt/node/prod
-%attr( 755 ,root, root) /opt/node/priv
-%attr( 755 ,root, root) /opt/node/test
+%attr( 755 ,root, root) /var/log/f3mon
+%attr( 755 ,root, root) /var/log/f3mon/prod
+%attr( 755 ,root, root) /var/log/f3mon/priv
+%attr( 755 ,root, root) /var/log/f3mon/test
+%attr( 755 ,root, root) /opt/f3mon/
+%attr( 755 ,root, root) /opt/f3mon/prod
+%attr( 755 ,root, root) /opt/f3mon/priv
+%attr( 755 ,root, root) /opt/f3mon/test
 %attr( 755 ,root, root) /usr/lib/systemd/system/f3mon.service
 %attr( 755 ,root, root) /usr/lib/systemd/system/f3mon.priv.service
 %attr( 755 ,root, root) /usr/lib/systemd/system/f3mon.test.service
-%attr( 755 ,root, root) /etc/logrotate.d/f3mon
+%attr( 644 ,root, root) /etc/logrotate.d/f3mon
 
 %post
 #echo "post install trigger"
@@ -93,6 +93,9 @@ cp $BASEDIR/logrotate-node %{buildroot}/etc/logrotate.d/f3mon
 /sbin/service fff-node-server stop || true
 /sbin/service priv-fff-node-server stop || true
 /sbin/service test-fff-node-server stop || true
+chkconfig --del fff-node-server || true
+chkconfig --del priv-fff-node-server || true
+chkconfig --del test-fff-node-server || true
 
 #main service stop:
 systemctl stop f3mon f3mon.priv f3mon.test || true
@@ -101,43 +104,43 @@ systemctl stop f3mon f3mon.priv f3mon.test || true
 rm -rf /var/run/*fff-node-server.pid
 
 #set symlinks to NFS
-unlink /opt/node/prod/app.js >& /dev/null || true
-unlink /opt/node/prod/node_modules >& /dev/null || true
-unlink /opt/node/prod/web >& /dev/null || true
-unlink /opt/node/prod/src >& /dev/null || true
-ln -s /cmsnfses-web/es-web/prod/app.js /opt/node/prod/app.js
-ln -s /cmsnfses-web/es-web/prod/node_modules /opt/node/prod/node_modules
-ln -s /cmsnfses-web/es-web/prod/web /opt/node/prod/web
-ln -s /cmsnfses-web/es-web/prod/src /opt/node/prod/src
+unlink /opt/f3mon/prod/app.js >& /dev/null || true
+unlink /opt/f3mon/prod/node_modules >& /dev/null || true
+unlink /opt/f3mon/prod/web >& /dev/null || true
+unlink /opt/f3mon/prod/src >& /dev/null || true
+ln -s /cmsnfses-web/es-web/prod/app.js /opt/f3mon/prod/app.js
+ln -s /cmsnfses-web/es-web/prod/node_modules /opt/f3mon/prod/node_modules
+ln -s /cmsnfses-web/es-web/prod/web /opt/f3mon/prod/web
+ln -s /cmsnfses-web/es-web/prod/src /opt/f3mon/prod/src
 
-unlink /opt/node/priv/app.js >& /dev/null || true
-unlink /opt/node/priv/node_modules >& /dev/null || true
-unlink /opt/node/priv/web >& /dev/null || true
-unlink /opt/node/priv/src >& /dev/null || true
-ln -s /cmsnfses-web/es-web/priv/app.js /opt/node/priv/app.js
-ln -s /cmsnfses-web/es-web/priv/node_modules /opt/node/priv/node_modules
-ln -s /cmsnfses-web/es-web/priv/web /opt/node/priv/web
-ln -s /cmsnfses-web/es-web/priv/src /opt/node/priv/src
+unlink /opt/f3mon/priv/app.js >& /dev/null || true
+unlink /opt/f3mon/priv/node_modules >& /dev/null || true
+unlink /opt/f3mon/priv/web >& /dev/null || true
+unlink /opt/f3mon/priv/src >& /dev/null || true
+ln -s /cmsnfses-web/es-web/priv/app.js /opt/f3mon/priv/app.js
+ln -s /cmsnfses-web/es-web/priv/node_modules /opt/f3mon/priv/node_modules
+ln -s /cmsnfses-web/es-web/priv/web /opt/f3mon/priv/web
+ln -s /cmsnfses-web/es-web/priv/src /opt/f3mon/priv/src
 
-unlink /opt/node/test/app.js >& /dev/null || true
-unlink /opt/node/test/node_modules >& /dev/null || true
-unlink /opt/node/test/web >& /dev/null || true
-unlink /opt/node/test/src >& /dev/null || true
-ln -s /cmsnfses-web/es-web/test/app.js /opt/node/test/app.js
-ln -s /cmsnfses-web/es-web/test/node_modules /opt/node/test/node_modules
-ln -s /cmsnfses-web/es-web/test/web /opt/node/test/web
-ln -s /cmsnfses-web/es-web/test/src /opt/node/test/src
+unlink /opt/f3mon/test/app.js >& /dev/null || true
+unlink /opt/f3mon/test/node_modules >& /dev/null || true
+unlink /opt/f3mon/test/web >& /dev/null || true
+unlink /opt/f3mon/test/src >& /dev/null || true
+ln -s /cmsnfses-web/es-web/test/app.js /opt/f3mon/test/app.js
+ln -s /cmsnfses-web/es-web/test/node_modules /opt/f3mon/test/node_modules
+ln -s /cmsnfses-web/es-web/test/web /opt/f3mon/test/web
+ln -s /cmsnfses-web/es-web/test/src /opt/f3mon/test/src
 
 #set user ownership
 /usr/sbin/useradd es-cdaq-runtime -g es-cdaq -s /sbin/nologin || true
 /usr/sbin/useradd es-cdaq-priv -g es-cdaq -s /sbin/nologin || true
 
-chown es-cdaq-runtime:es-cdaq -R /var/log/node/prod/*.log || true
-chown es-cdaq-priv:es-cdaq -R /var/log/node/priv/*.log || true
-chown es-cdaq-runtime:es-cdaq -R /var/log/node/test/*.log || true
+chown es-cdaq-runtime:es-cdaq -R /var/log/f3mon/prod/*.log >& /dev/null || true
+chown es-cdaq-priv:es-cdaq -R /var/log/f3mon/f3mon/*.log >& /dev/null || true
+chown es-cdaq-runtime:es-cdaq -R /var/log/f3mon/f3mon/*.log >& /dev/null || true
 
 systemctl daemon-reload
-systemctl enable f3mon f3mon.priv f3mon.test
+systemctl reenable f3mon f3mon.priv f3mon.test
 systemctl restart f3mon f3mon.priv f3mon.test
 
 %preun
@@ -150,17 +153,17 @@ if [ \$1 == 0 ]; then
   /usr/sbin/userdel es-cdaq-runtime || true
   /usr/sbin/userdel es-cdaq-priv || true
 
-  unlink /opt/node/prod/app.js >& /dev/null || true
-  unlink /opt/node/prod/node_modules >& /dev/null || true
-  unlink /opt/node/prod/web >& /dev/null || true
+  unlink /opt/f3mon/prod/app.js >& /dev/null || true
+  unlink /opt/f3mon/prod/node_modules >& /dev/null || true
+  unlink /opt/f3mon/prod/web >& /dev/null || true
 
-  unlink /opt/node/priv/app.js >& /dev/null || true
-  unlink /opt/node/priv/node_modules >& /dev/null || true
-  unlink /opt/node/priv/web >& /dev/null || true
+  unlink /opt/f3mon/priv/app.js >& /dev/null || true
+  unlink /opt/f3mon/priv/node_modules >& /dev/null || true
+  unlink /opt/f3mon/priv/web >& /dev/null || true
 
-  unlink /opt/node/test/app.js >& /dev/null || true
-  unlink /opt/node/test/node_modules >& /dev/null || true
-  unlink /opt/node/test/web >& /dev/null || true
+  unlink /opt/f3mon/test/app.js >& /dev/null || true
+  unlink /opt/f3mon/test/node_modules >& /dev/null || true
+  unlink /opt/f3mon/test/web >& /dev/null || true
 
 fi
 
